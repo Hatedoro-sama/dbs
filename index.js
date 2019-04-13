41fe141d06a45bd9e24c328a45ff42fa956cb00c
Hatedoro-sama <49577219+Hatedoro-sama@users.noreply.github.com>
2019-04-13 16:21:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const cfg require('./index.json'); // a garder en version desktop
 const token = process.env.token // a garder en version heroku
 const prefix = ("?");
 
